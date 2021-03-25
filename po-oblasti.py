b4222e88e829a998bdd7ac010b6727d8fa6f400e
Andrew Ivanov <nik1297@gmail.com>
2021-03-25 18:02:31 +0300

    запускал в пн 13-01-2020 20.03

---
+++ po-oblasti.py
@@ -0,0 +1,4 @@
+# собрать данные о компаниях в данной области
+# сначала собираем инфу по фирмам
+# потом смотрим все вакансии этих фирм и заносим в файлик
+# https://hh.ru/employers_company/medicina_farmacevtika_apteki?vacanciesRequired=true
